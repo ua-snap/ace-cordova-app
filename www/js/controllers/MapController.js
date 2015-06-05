ec2af59562b347865f300faf9f50926a4941339d
James Sentell <jamessentell1@gmail.com>
2015-06-05 07:44:50 -0500

    Changed maps view to track location (by dropping a new marker and re-centering the map on location updates).  Also added scale control.

--- www/js/controllers/MapController.js
+++ www/js/controllers/MapController.js
@@ -19,36 +19,43 @@ angular.module('starter.controllers')
 	// regardless of controller and state caching.
 	$scope.$on('$ionicView.enter', function() {
 		$ionicSideMenuDelegate.canDragContent(false);
+        
+        initialize();
 	});
     
-    // default latitude and longitude
-    var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
-
-    // Set up map options
-    var mapOptions = {
-        center: myLatlng,
-        zoom: 16,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    // Create map
-    var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    var initialize = function()
+    {
+        // default latitude and longitude
+        var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
     
-    // Get current position and update map once position is recieved
-    navigator.geolocation.getCurrentPosition(function(pos) {
-        map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-        var myLocation = new google.maps.Marker({
-            position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
-            map: map,
-            title: "My Location"
-        });
-    }, function(error) {
-        alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
-    }, {timeout: 10000, enableHighAccuracy: true});
+        // Set up map options
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            scaleControl: true,
+            panControl: false
+        };
+    
+        // Create map
+        var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+        
+        // Get current position and update map once position is recieved
+        navigator.geolocation.watchPosition(function(pos) {
+            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            var myLocation = new google.maps.Marker({
+                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                map: map,
+                title: "My Location",
+            });
+        }, function(error) {
+            alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+        }, {timeout: 10000, enableHighAccuracy: true});
+        
+         $scope.map = map;
+    };   
+    
+   
 
-
-
-    // Save the map for later access
-    $scope.map = map;
 });
