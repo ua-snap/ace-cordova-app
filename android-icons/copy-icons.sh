7688e0c742efcce10dc6275cd7b940e46268eba4
jamessentell <sentelljg@SentellJG-Mac.local>
2015-09-04 08:47:05 -0500

    Adding script to setup notification icons

---
+++ android-icons/copy-icons.sh
@@ -0,0 +1,10 @@
+# Copy all required icon files
+cp -f ic_cloud_done_white_24dp/android/drawable-hdpi/ic_cloud_done_white_24dp ../platforms/android/res/drawable-hdpi
+cp -f ic_cloud_done_white_24dp/android/drawable-mdpi/ic_cloud_done_white_24dp ../platforms/android/res/drawable-mdpi
+cp -f ic_cloud_done_white_24dp/android/drawable-xhdpi/ic_cloud_done_white_24dp ../platforms/android/res/drawable-xhdpi
+cp -f ic_cloud_done_white_24dp/android/drawable-xxhdpi/ic_cloud_done_white_24dp ../platforms/android/res/drawable-xxhdpi
+
+cp -f ic_cloud_white_24dp/android/drawable-hdpi/ic_cloud_done_white_24dp ../platforms/android/res/drawable-hdpi
+cp -f ic_cloud_white_24dp/android/drawable-mdpi/ic_cloud_done_white_24dp ../platforms/android/res/drawable-mdpi
+cp -f ic_cloud_white_24dp/android/drawable-xhdpi/ic_cloud_done_white_24dp ../platforms/android/res/drawable-xhdpi
+cp -f ic_cloud_white_24dp/android/drawable-xxhdpi/ic_cloud_done_white_24dp ../platforms/android/res/drawable-xxhdpi
